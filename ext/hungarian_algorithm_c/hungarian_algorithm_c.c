188c3df2c614c654fc6afbbe3a9490917583ad1a
Humza [Syed H. Shah] <humzashah@gmail.com>
2017-05-05 10:41:15 +0100

    remove multiplication by 100

--- ext/hungarian_algorithm_c/hungarian_algorithm_c.c
+++ ext/hungarian_algorithm_c/hungarian_algorithm_c.c
@@ -53,7 +53,7 @@ VALUE pairs(VALUE self, VALUE flattened_array_ruby, VALUE row_size_val) {
 
   int index;
   for (index = 0; index < array_size; index++) {
-    long double element = 100 * NUM2DBL(rb_ary_entry(flattened_array_ruby, index));
+    long double element = NUM2DBL(rb_ary_entry(flattened_array_ruby, index));
     long long int rounded_element = element;
     array_c[index] = rounded_element;
   }
